05c6c7a4a0cb86f03cbb83ca6ad4c1ad60a9c51b
niraekjs <35584613+niraekjs@users.noreply.github.com>
2022-08-29 18:38:45 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -48,18 +48,18 @@ with open("./README.md") as readme:
 
 
 setup(
-    name="eth-account",
+    name="eth-account-n",
     # *IMPORTANT*: Don't manually change the version here. Use `make bump`, as described in readme
-    version="0.7.0",
+    version="0.0.2",
     description="""eth-account: Sign Ethereum transactions and messages with local private keys""",
     long_description=long_description,
     long_description_content_type="text/markdown",
     author="The Ethereum Foundation",
     author_email="snakecharmers@ethereum.org",
-    url="https://github.com/ethereum/eth-account",
+    url="https://github.com/niraekjs/eth-account-n",
     include_package_data=True,
     package_data={
-        "eth_account": [
+        "eth_account-n": [
             "py.typed",
             "hdaccount/wordlist/*.txt",
         ]
